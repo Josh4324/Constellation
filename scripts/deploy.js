dc5be4f3417d14da77c7e78a40b9f7149ce7582c
Josh <adesanyajoshua@ymail.com>
2023-11-28 16:40:01 +0100

    add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,26 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const eco = await hre.ethers.deployContract(" ECO4Reward", []);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await eco.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  console.log(`Contract deployed to ${eco.target}`);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: eco.target,
+    constructorArguments: [],
+    contract: "contracts/ECO.sol:ECO4Reward",
   });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
