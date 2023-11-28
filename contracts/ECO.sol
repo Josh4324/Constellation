dc277e6ca567a8af02f050418ff8f991e4be7d0e
Josh <adesanyajoshua@ymail.com>
2023-11-28 16:37:09 +0100

    initial commit

---
+++ contracts/ECO.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+//import "hardhat/console.sol";
+
+/**
+ * @title A ECO4Reward Contract
+ * @author Joshua Adesanya
+ * @notice This contract is for managing and rewarding environmental actions.
+ */
+
+contract ECO4Reward {}
